e28e37270126970c9e8adb9d24d3877f31ac4560
alboy77 <al.boucetta@yahoo.com>
2023-04-20 18:59:00 -0400

    gotta figirue out the json parse ugh

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path =  require("path");
 const fs = require("fs");
 const notes = require("./db/db.json");
 
+const generateUniqueId = require("generate-unique-id");
+const id = generateUniqueId();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -21,8 +24,25 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request to add note`)
-    console.log(`${req.method} request for notes`)
+    res.status(200).json(`${req.method} request to add note`);
+    console.info(`${req.method} request to add note`);
+
+    const {title, text} = req.body
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: generateUniqueId(),
+        };
+
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const
+            }
+        })
+    }
 
 })
 
