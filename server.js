c481d39909e2965da9e80be32a9bfcd3a27bcf63
alboy77 <al.boucetta@yahoo.com>
2023-04-20 18:02:10 -0400

    app.get maybe right? hopefully

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const notes = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err,data) => {
+        if (err) {
+            throw (err)
+        }
+        res.status(200).json(JSON.parse (data));
+    })
+    console.log(`${req.method} request for notes`)
+});
+
 // app.get('notes??')
 // send file?
 
@@ -13,3 +27,4 @@ const app = express();
 app.listen(PORT, () => 
     console.log('http://localhost:${PORT}')
 );
+
