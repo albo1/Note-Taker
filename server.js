0909195d1371e613a0c02d45338469170af866c8
alboy77 <al.boucetta@yahoo.com>
2023-04-20 20:50:39 -0400

    delete btn makes me want to cry

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path =  require("path");
+const path = require("path");
 const fs = require("fs");
 const notes = require("./db/db.json");
 
@@ -10,15 +10,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err,data) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) {
             throw (err)
         }
-        res.status(200).json(JSON.parse (data));
+        res.status(200).json(JSON.parse(data));
     })
     console.info(`${req.method} request for notes`)
 });
@@ -27,7 +27,7 @@ app.post('/api/notes', (req, res) => {
     res.status(200).json(`${req.method} request to add note`);
     console.info(`${req.method} request to add note`);
 
-    const {title, text} = req.body
+    const { title, text } = req.body
     if (title && text) {
         const newNote = {
             title,
@@ -43,26 +43,47 @@ app.post('/api/notes', (req, res) => {
                 notesParsed.push(newNote);
 
                 fs.writeFile('./db/db.json', JSON.stringify(notesParsed),
-                (writeError) => 
-                    writeError ? console.error(writeError) : console.info('Sucessfully Updated Notes!')
+                    (writeError) =>
+                        writeError ? console.error(writeError) : console.info('Sucessfully Updated Notes!')
                 );
             }
         });
-        const response = { 
+        const response = {
             status: 'success',
             body: newNote,
         };
     }
-})
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw (err)
+        } else {
+            const notesParsed = JSON.parse(data)
+            const newNotesList = notesParsed.filter(note => note.id !== noteId)
+
+            fs.writeFile('./db/db.json', JSON.stringify(newNotesList),
+                (writeError) =>
+                    writeError ? console.error(writeError) : console.info('Successfully updated notes!')
+            );
+        }
+    });
+    res.status(200).send('Note Deleted!');
+});
+
+
+
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-    );
+);
 
-app.get('*', (req, res) => 
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    );
+);
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`APP LISTENING AT http://localhost:${PORT}`)
 );
 
